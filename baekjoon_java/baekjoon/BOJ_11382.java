6128f3fbd83c5556ee41f26d7bb1da4c11005b81
ddugi <juju80203@gmail.com>
2026-04-08 23:50:34 +0900

    백준 11382

---
+++ baekjoon_java/baekjoon/BOJ_11382.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A,B,C;
+        A = sc.nextLong();
+        B = sc.nextLong();
+        C = sc.nextLong();
+        System.out.println(A+B+C);
+    }
+}
